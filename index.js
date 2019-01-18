d0c7c1f6c61f1940c246f03959f302079350e792
Billy Witherspoon <wwither@gmail.com>
2019-01-18 05:46:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+function takeANumber(katzDeliLine) {
+  var i = katzDeliLine.length;
+  var j = katzDeliLine[i];
+  katzDeliLine.push(j + 1);
+  return (`Welcome, You are number ${katzDeliLine.length} in line.`);
 }
 
 function nowServing(katzDeliLine){
